1ac71ce98fa7d14e6a9740235be2ce2825a624bb
Amanda Rawls <22384389+amandarawls@users.noreply.github.com>
2019-03-18 21:40:30 -0500

    answers

--- Submissions/Rawls.playground/Contents.swift
+++ Submissions/Rawls.playground/Contents.swift
@@ -1,21 +1,31 @@
+//: Simple Values
+//: 1. Create a constant with an explicit type of Float that equals 4.
+let myFloat: Float = 4.0
 
-
-
-
+//: 2. Try removing the conversion to String from the last line. Copy the error and place it in a block comment below this block of code.
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+/* Binary operator '+' cannot be applied to operands of type 'String' and 'Int' */
 //: 3. Use \() to include a floating-point calculation in a string and to include someone's name in a greeting.
-
+let floatingPointCalculationString = "I am adding 3.4 + 1.2 to get \(3.4 + 1.2)"
+let someonesName = "Amanda"
+let greetingSomeone = "Hello, \(someonesName)"
 
 //: Control Flow
 //: 1. Change optional Name to nil. What greeting do you get? Add an else clause that sets a different greeting if optionalName is nil.
-var optionalString: String? = "Bonjour"
+var optionalString: String? = "Hello"
 print(optionalString == nil)
 
-var optionalName: String? = "John Appleseed"
+var optionalName: String? = nil
 var greeting = "Hello!"
 if let name = optionalName {
     greeting = "Hello, \(name)"
+} else {
+    greeting = "Bonjour!"
 }
 
+// changing optionalName to nil causes no greeting to print.
 //: 2. Try removing the default case. Copy the error and place it in a block comment below this block of code.
 let vegetable = "red pepper"
 switch vegetable {
@@ -29,6 +39,7 @@ default:
     print("Everything tastes good in soup.")
 }
 
+/* Switch must be exhaustive */
 //: 3. Add another variable to keep track of which kind of number was the largest, as well as what that largest number was.
 
 let interestingNumbers = [
@@ -37,11 +48,14 @@ let interestingNumbers = [
     "Square": [1, 4, 9, 16, 25],
 ]
 var largest = 0
+var kindOfNumber = ""
 for (kind, numbers) in interestingNumbers {
     for number in numbers {
         if number > largest {
             largest = number
+            kindOfNumber = kind
         }
     }
 }
 print(largest)
+print(kindOfNumber)
